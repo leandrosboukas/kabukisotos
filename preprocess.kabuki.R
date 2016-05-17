08fa1a9eac19f59b03686ca93508004e59090d5f
leandrosboukas <leandrosboukas@gmail.com>
2016-05-17 18:54:20 +0300

    preprocess.kabuki.R

---
+++ preprocess.kabuki.R
@@ -0,0 +1,8 @@
+load(file="/dcl01/hansen/data/meth_kabuki_bjornsson/objects/rgSet.kabuki.rda")
+grSet.kabuki<-preprocessQuantile(rgSet.kabuki)
+
+###keep only whole blood samples from Control and Sotos individuals
+
+grSet.kabuki<-grSet.kabuki[,grSet.kabuki$Phenotype %in% c("normal","KS"),drop=FALSE]
+grSet.kabuki<-grSet.kabuki[,grSet.kabuki$KnownGeneMutations %in% c("NoneKnown","MLL2","MLL","MLL4"),drop=FALSE]
+save(grSet.kabuki, file="/dcl01/hansen/data/meth_kabuki_bjornsson/objects/grSet.kabuki.rda")
