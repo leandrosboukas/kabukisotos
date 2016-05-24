cd5a93fdeb777adbc1af68ab3bff447cb46f07f8
leandrosboukas <leandrosboukas@gmail.com>
2016-05-24 18:39:52 +0300

    Update process.chouf.R

--- process.chouf.R
+++ process.chouf.R
@@ -2,8 +2,8 @@ load(file="/dcl01/hansen/data/meth_sotos/objects/rgSet.chouf.rda")
 ##now I'm gonna drop the Sotos samples from Hong Kong because they weren't used in the DMP analysis, they were only used later for validation of the methylation signature
 hklist<-which(substr(pData(rgSet.chouf)$title,1,2)=="HK") 
 hklist<-hklist[-(20:29)]
-##now drop keep only control whole blood and sotos whole blood samples
 rgSetinit<-rgSet[,-hklist,drop=FALSE]
+##now keep only control whole blood and sotos whole blood samples
 rgSetinit<-rgSetinit[,which(pData(rgSetinit)$group%in%c("Control","Sotos")),drop=FALSE]
 rgSetinit<-rgSetinit[,which(pData(rgSetinit)$tissue%in%c("whole blood")),drop=FALSE]
 ##now create three different rgSets because they did 3 comparisons of the 53 controls with 17 sotos patients, each time containing 16 sotos and one of the three family members who all had sotos
